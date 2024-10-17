2e7573bc37644869be6d217521b5cb50cf1a746a
Ana Isakov <aisakov8709@conestogac.on.ca>
2024-10-17 14:34:20 -0400

    Develop the BookController with CRUD operations
    To add, edit, or delete a book; or
    To open the Book List or Details pages

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,12 +1,86 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookManagementApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookManagementApp.Controllers
 {
 	public class BookController : Controller
 	{
-		public IActionResult Index()
+		// Context property of Controller
+		public BookContext Context { get; set; }
+
+		// Constructor of the Controller
+		public BookController(BookContext context)
 		{
-			return View();
+			Context = context;
+		}
+
+		// BookList (index) page
+		public IActionResult BookList()
+		{
+			var books = Context.Books.Include(b => b.Genre).OrderBy(b => b.Title).ToList();
+			return View(books);
+		}
+
+		// Book Details page
+		public IActionResult Details(int id)
+		{
+			var book = Context.Books.Include(b => b.Genre).FirstOrDefault(b => b.Id == id);
+			return View(book);
+		}
+
+		// Adding a Book
+		[HttpGet]
+		public IActionResult AddBook()
+		{
+			ViewBag.Genres = Context.Genres.OrderBy(g => g.Name).ToList();
+			return View("EditBook", new Book());
+		}
+
+		// Editing a Book
+		[HttpGet]
+		public IActionResult EditBook(int id)
+		{
+			ViewBag.Genres = Context.Genres.OrderBy(g => g.Name).ToList();
+			var bookId = Context.Books.FirstOrDefault(b => b.Id == id);
+			return View(bookId);
+		}
+		[HttpPost]
+		public IActionResult EditBook(Book book)
+		{
+			if (ModelState.IsValid)
+			{
+				if (book.Id == 0)
+				{
+					Context.Books.Add(book);
+				}
+				else
+				{
+					Context.Books.Update(book);
+				}
+				Context.SaveChanges();
+				return RedirectToAction("BookList");
+			}
+			else
+			{
+				ViewBag.Genres = Context.Genres.OrderBy(g => g.Name).ToList();
+				return View(book);
+			}
+		}
+
+		// Deleting a Book
+		[HttpGet]
+		public IActionResult DeleteBook(int id)
+		{
+			var bookId = Context.Books.FirstOrDefault(b => b.Id == id);
+			return View(bookId);
+		}
+		[HttpPost]
+		public IActionResult DeleteBook(Book book)
+		{
+			Context.Books.Remove(book);
+			Context.SaveChanges();
+			return RedirectToAction("BookList");
 		}
 	}
 }
