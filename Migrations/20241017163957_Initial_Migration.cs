d95c549cf3b7476c5eccc787e5f6683ad05cd256
Ana Isakov <aisakov8709@conestogac.on.ca>
2024-10-17 13:55:42 -0400

    Finish setting up Entity Framework through Migrations
    Completed migrations and added a connection string to appsettings.json

---
+++ Migrations/20241017163957_Initial_Migration.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BookManagementApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial_Migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    GenreId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.GenreId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    GenreId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    IsAvailable = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Books_Genres_GenreId",
+                        column: x => x.GenreId,
+                        principalTable: "Genres",
+                        principalColumn: "GenreId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Genres",
+                columns: new[] { "GenreId", "Name" },
+                values: new object[,]
+                {
+                    { "A", "Action" },
+                    { "C", "Comedy" },
+                    { "D", "Drama" },
+                    { "M", "Musical" },
+                    { "N", "Nature" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "GenreId", "IsAvailable", "Title", "Year" },
+                values: new object[,]
+                {
+                    { 1, "Edward Kolla", "A", true, "Koala Adventure", 2007 },
+                    { 2, "Maria Von Trapp", "M", true, "The Sound of Music", 1950 },
+                    { 3, "Kenneth Kolla", "N", false, "Koalas", 2010 },
+                    { 4, "Edward Kolla", "C", true, "Koala Comedy", 2014 },
+                    { 5, "Edward Kolla", "D", true, "Koala Drama", 2017 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_GenreId",
+                table: "Books",
+                column: "GenreId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Genres");
+        }
+    }
+}
